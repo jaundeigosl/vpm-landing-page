1bba9755916613eba2f05e76fcb251d304d6cef1
frog <juandiegosuarez00@gmail.com>
2025-07-02 17:45:53 -0400

    contactanos section

--- js/main.js
+++ js/main.js
@@ -65,32 +65,10 @@
 	// Reflect scrolling in navigation
 	var navActive = function(section) {
 
-		var $el = $('#navbar > ul');
-		$el.find('li').removeClass('active');
-		
-
 	};
 
 	var navigationSection = function() {
 
-		var $section = $('section[data-section]');
-		
-		$section.waypoint(function(direction) {
-		  	
-		  	if (direction === 'down') {
-		    	navActive($(this.element).data('section'));
-		  	}
-		}, {
-	  		offset: '150px'
-		});
-
-		$section.waypoint(function(direction) {
-		  	if (direction === 'up') {
-		    	navActive($(this.element).data('section'));
-		  	}
-		}, {
-		  	offset: function() { return -$(this.element).height() + 155; }
-		});
 
 	};
 
